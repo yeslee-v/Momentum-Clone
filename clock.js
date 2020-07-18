9a002594ef10cab81a91c76d2493f8d44b871b5e
yeslee-v <yeslee5004@gmail.com>
2020-07-18 09:59:52 +0900

    add remarks

--- clock.js
+++ clock.js
@@ -1,4 +1,4 @@
-const clockContainer = document.querySelector(".js-clock"),
+const clockContainer = document.querySelector(".js-clock"), // querySelector: element의 자식 탐색
   clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
@@ -8,12 +8,12 @@ function getTime() {
   const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }:${seconds < 10 ? `0${seconds}` : seconds}`; // 질문 ? True : false
 }
 
 function init() {
   getTime();
-  setInterval(getTime, 1000);
+  setInterval(getTime, 1000); // setInterval(실행할 함수, 실행 시간(millisecond) 간격)
 }
 
-init();
+init(); // 함수 초기화
